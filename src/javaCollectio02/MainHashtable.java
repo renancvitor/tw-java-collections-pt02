0148bb9edcfe22a7ee8e86d74fbce2810aa6b8ac
renancvitor <renan.vitor.cm@gmail.com>
2025-06-27 16:14:22 -0300

    feat(curso): aula sobre Hashtable com Iterator e EnumEnumeration

--- src/javaCollectio02/MainHashtable.java
+++ src/javaCollectio02/MainHashtable.java
@@ -1,12 +1,15 @@
 package javaCollectio02;
 
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
+
 import modelCollection.Pessoa;
 
 public class MainHashtable {
 
     public static void main(String[] args) {
-        Hashtable<String,Pessoa> tabelaPessoas = new Hashtable<String, Pessoa>();
+        Hashtable<String,Pessoa> tabelaPessoas = new Hashtable<String, Pessoa>(20, 0.6f);
         tabelaPessoas.put("Legal", new Pessoa(1, "TreinaWeb 1"));
         System.out.println(tabelaPessoas);
         tabelaPessoas.put("Chato", new Pessoa(2, "TreinaWeb 2"));
@@ -14,5 +17,22 @@ public class MainHashtable {
         System.out.println(tabelaPessoas.get("Legal"));
         tabelaPessoas.put("Legal", new Pessoa(3, "TreinaWeb 3"));
         System.out.println(tabelaPessoas.get("Legal"));
+        tabelaPessoas.put("Bla", new Pessoa(4, "TreinaWeb 4"));
+
+        // Iteração
+        // Fail-fast Iterator
+//        Iterator<String> iteratorChaves = tabelaPessoas.keySet().iterator();
+//        tabelaPessoas.remove("Legal");
+//        while (iteratorChaves.hasNext()) {
+//            String chave = iteratorChaves.next();
+//            System.out.println(" - " + tabelaPessoas.get(chave).toString());
+//        }
+        // Non Fail-fast Iterator
+        Enumeration<String> chaves = tabelaPessoas.keys();
+        tabelaPessoas.remove("Legal");
+        while (chaves.hasMoreElements()) {
+            String chave = chaves.nextElement();
+            System.out.println(" - " + tabelaPessoas.get(chave).toString());
+        }
     }
 }
