1557774ea03016ad93ae446ca12518086f3febab
renancvitor <renan.vitor.cm@gmail.com>
2025-06-30 13:51:08 -0300

    feat(curso): aula sobre LinkedHashMap

--- src/javaCollectio02/MainMap.java
+++ src/javaCollectio02/MainMap.java
@@ -1,6 +1,7 @@
 package javaCollectio02;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import modelCollection.Pessoa;
 
@@ -8,7 +9,8 @@ public class MainMap {
 
     public static void main(String[] args) {
 
-        Map<String, Pessoa> mapaPessoa = new HashMap<String, Pessoa>();
+        // Map<String, Pessoa> mapaPessoa = new HashMap<String, Pessoa>(); // Não garante ordem, "mais leve"
+        Map<String, Pessoa> mapaPessoa = new LinkedHashMap<String, Pessoa>(16, 0.75f, true); // Garante ordem, "mais pesado"
         System.out.println(mapaPessoa.put("Legal", new Pessoa(1, "TreinaWeb 1")));
         System.out.println(mapaPessoa);
         System.out.println(mapaPessoa.put("Legal", new Pessoa(2, "TreinaWeb 2")));
@@ -18,6 +20,9 @@ public class MainMap {
         mapaPessoa.put(null, new Pessoa(-2, "TreinaWeb N U L O {2}"));
         System.out.println(mapaPessoa);
         mapaPessoa.put("Teste", null);
+        System.out.println(mapaPessoa);
+        Pessoa p = mapaPessoa.get("Legal");
+        System.out.println(p);
         System.out.println(mapaPessoa);
     }
 }
